a74e365186e744cf1554bef65ab2a0d51204836b
Yvonne Ceelie <yvonne.ceelie@axoniq.io>
2020-11-01 17:23:08 +0100

    No exception for tracing when running locally

---
+++ inventory/src/main/java/io/axoniq/demo/hotel/inventory/config/TracingConfig.java
@@ -0,0 +1,26 @@
+package io.axoniq.demo.hotel.inventory.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.jaegertracing.internal.JaegerTracer;
+import io.jaegertracing.internal.reporters.InMemoryReporter;
+import io.jaegertracing.internal.samplers.ConstSampler;
+import io.jaegertracing.spi.Reporter;
+import io.jaegertracing.spi.Sampler;
+
+@ConditionalOnProperty(value = "opentracing.jaeger.enabled", havingValue = "false", matchIfMissing = false)
+@Configuration
+public class TracingConfig {
+
+    @Bean
+    public io.opentracing.Tracer jaegerTracer() {
+        final Reporter reporter = new InMemoryReporter();
+        final Sampler sampler = new ConstSampler(false);
+        return new JaegerTracer.Builder("untraced-service")
+                .withReporter(reporter)
+                .withSampler(sampler)
+                .build();
+    }
+}
