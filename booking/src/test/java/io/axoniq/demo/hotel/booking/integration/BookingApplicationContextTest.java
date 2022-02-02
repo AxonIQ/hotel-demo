1eff0248860087a0c46e2f82499903dc4c81d976
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2022-02-02 15:08:51 +0100

    Upgrade dependencies and add Spring boot tests to ensure it boots

---
+++ booking/src/test/java/io/axoniq/demo/hotel/booking/integration/BookingApplicationContextTest.java
@@ -0,0 +1,12 @@
+package io.axoniq.demo.hotel.booking.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookingApplicationContextTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
